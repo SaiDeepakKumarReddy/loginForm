5d56720bfc49522ca116151547ba6f7276930fda
SaiDeepakKumarReddy <ksaideepak123@gmail.com>
2024-07-08 23:47:25 -0700

    first commit

---
+++ form/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Signup from './Signup'
+const App = () => {
+  return (
+    <div>
+     <Signup/>
+    </div>
+  )
+}
+export default App
